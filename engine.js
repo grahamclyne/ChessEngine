6ebc7d31e30cf9eafa458dd6475ea9aa39f3e8c4
Graham Clyne <clyne.graham@gmail.com>
2020-10-12 19:14:59 -0400

    adding basic bitmaps

--- engine.js
+++ engine.js
@@ -1,3 +1,46 @@
+const bs = require('bitset');
+
 var lib = require('./lib.js')
 
-lib.setRankMasks(0,[]);
+var rankMasks = lib.setRankMasks(0,[]);
+var kingMap = new bs.BitSet();
+var WP = new bs.BitSet();
+var WR = new bs.BitSet();
+var WK = new bs.BitSet();
+var WB = new bs.BitSet();
+var WQ = new bs.BitSet();
+var WK = new bs.BitSet();
+
+var BP = new bs.BitSet();
+var BR = new bs.BitSet();
+var BK = new bs.BitSet();
+var BB = new bs.BitSet();
+var BQ = new bs.BitSet();
+var BK = new bs.BitSet();
+
+WP.setRange(8,15,1);
+WR.set(0,1);
+WR.set(7,1);
+WK.set(1,1);
+WK.set(6,1);
+WB.set(2,1);
+WB.set(5,1);
+WQ.set(4,1);
+WK.set(5,1);
+
+BP.setRange(48,55,1);
+BR.set(0+56,1);
+BR.set(7+56,1);
+BK.set(1+56,1);
+BK.set(6+56,1);
+BB.set(2+56,1);
+BB.set(5+56,1);
+BQ.set(4+56,1);
+BK.set(5+56,1);
+
+
+var fileMasks = lib.setFileMasks(0, [])
+//fileMasks.map(x => console.log(x.toString(2)))
+var pawns = BP.or(WP)
+var WPPossibilities = lib.rightShift(WP,7).or(lib.rightShift(WP,9))
+console.log(WPPossibilities.toString(2))
--- lib.js
+++ lib.js
@@ -8,7 +8,7 @@ const R = require("ramda");
 
 exports.setRankMasks = function (x, rankMasks) {
     var rank = new bs.BitSet();
-    while(x < 65) {
+    while(x < 64) {
         if((x+1) % 8 == 0 && x != 0) {
             rankMasks.push(rank.set(x,1).clone());
             rank.clear();
@@ -18,6 +18,53 @@ exports.setRankMasks = function (x, rankMasks) {
         }
         x = x + 1;
     }
+    return rankMasks;
+}
+
+exports.setFileMasks = function( x, fileMasks) {
+    var file = new bs.BitSet();
+    y = 0
+    while(y < 8) {
+        x = y
+        while(x < 64) {
+            file.set(x,1);
+            x = x + 8 
+        }
+        fileMasks.push(file.clone());
+        file.clear();
+        y = y + 1;
+    }
+    return fileMasks
+}
+
+exports.leftShift = function (bitSet, distance) {
+    var lsb = bitSet.lsb()
+    var msb = bitSet.msb()
+    var output = new bs.BitSet()
+    while(lsb < msb){
+        var isSet = bitSet.get(lsb)
+        if(isSet){
+            output.set((lsb - distance), 1)
+        }
+        lsb = lsb + 1
+    }
+    return output
+}
+
+exports.rightShift = function (bitSet, distance) {
+    var lsb = bitSet.lsb()
+    var msb = bitSet.msb()
+    var output = new bs.BitSet()
+    while(lsb < msb){
+        var isSet = bitSet.get(lsb)
+        if(isSet){
+            output.set((lsb + distance), 1)
+        }
+        lsb = lsb + 1
+    }
+    return output
+}
+
+exports.whitePawnPossibilities(WP){
     
-    rankMasks.map(x => console.log(x.toString()));
 }
